424e672d09071ba2f7d6ac5d57f35da67d8f605f
sagar <chavansagar96044@gmail.com>
2022-07-30 13:24:24 +0530

    change sequence of players

--- src/com/bridgelabz/DeckOfCards.java
+++ src/com/bridgelabz/DeckOfCards.java
@@ -29,14 +29,43 @@ public class DeckOfCards {
             numOfPlayers = scanner.nextInt();
         } while (numOfPlayers < 2 || numOfPlayers > 4);
         for (int i = 0; i < numOfPlayers; i++) {
-            Player.addPlayer(new Player());
+            System.out.println("enter new planer name");
+            String playerName=scanner.next();
+            Player.addPlayer(new Player(playerName));
         }
         System.out.println("players added: "+numOfPlayers);
+    }
+
+    void changePlayerOrder(){
+        System.out.println(Player.playerList);
+        System.out.print("Do you want to change Player Order (y/n): ");
+
+        if (scanner.next().equalsIgnoreCase("y")){
+            System.out.println("sequencing the Players order..");
+            int n=Player.playerList.size();
+            for (int i = 0; i < n; i++) {
+                Player player=null;
+                do {
+                    System.out.println("enter player-" + (i + 1) + " name");
+                    player = Player.getPlayer(scanner.next());
+                    swapPlayer(i, Player.playerList.indexOf(player));
+                }while (player == null);
+
+            }
+        }
+
+    }
+    public void swapPlayer(int i, int j){
+        Player player=Player.playerList.get(i);
+        Player.playerList.add(i,Player.playerList.get(j));
+        Player.playerList.add(j,player);
     }
 
     public static void main(String[] args) {
         DeckOfCards deckOfCards=new DeckOfCards();
         deckOfCards.initializeCards();
         deckOfCards.addPlayers();
+        deckOfCards.changePlayerOrder();
+
     }
 }
--- src/com/bridgelabz/Player.java
+++ src/com/bridgelabz/Player.java
@@ -3,10 +3,47 @@ package com.bridgelabz;
 import java.util.ArrayList;
 
 public class Player {
+
+    private String name;
     Card[] playerCards =new Card[9];
     static ArrayList<Player> playerList=new ArrayList<>();
 
+    public Player(String name) {
+        this.name = name;
+    }
+
     public static void addPlayer(Player player){
         playerList.add(player);
+    }
+
+    public static Player getPlayer(String name){
+        Player player1=null;
+        for (Player player:playerList) {
+            if (player.getName().equalsIgnoreCase(name)) {
+                player1 = player;
+                break;
+            }
+        }
+        return player1;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Card[] getPlayerCards() {
+        return playerCards;
+    }
+
+    public void setPlayerCards(Card[] playerCards) {
+        this.playerCards = playerCards;
+    }
+
+    @Override
+    public String toString() {
+        return name + ' ';
     }
 }

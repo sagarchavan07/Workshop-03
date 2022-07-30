e4b8d77db341e7c0f03fcdec9154319303b0e422
sagar <chavansagar96044@gmail.com>
2022-07-30 15:15:46 +0530

    Display the player cards in order of sequence

--- src/com/bridgelabz/Card.java
+++ src/com/bridgelabz/Card.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
-public class Card {
+public class Card implements Comparable{
     public static final String[] suitArray =new String[]{"Clubs", "Diamonds", "Hearts", "Spades"};
     public static final String[] rankArray =new String[]{"2", "3", "4", "5", "6", "7", "8","9", "10", "Jack", "Queen", "King", "Ace"};
     public static Card[] cardArray =new Card[52];
@@ -27,5 +27,20 @@ public class Card {
 
     public void setRank(String rank) {
         this.rank = rank;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                suit + '\'' +
+                "-" + rank + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        int x=this.rank.hashCode();
+        int y=((Card)o).rank.hashCode();
+        return (x > y) ? 1 : (x == y) ? 0 : -1;
     }
 }
